037447f3452d20158bbe3c84816ececf56411df2
jvvitorcs <jvvitorcs97@gmail.com>
2024-10-24 18:51:47 -0300

    Vitor commit 002 Station Test - Deleting unnecessary punctuation marks, Adding documentation instructions and Test cases file

--- cypress/e2e/StationTest.cy.ts
+++ cypress/e2e/StationTest.cy.ts
@@ -1,3 +1,10 @@
+// ***********************************************************
+// Todos os casos de testes estão no arquivo Excell Growth_Station_TestCase
+// A única ação necessário para executar a automação é executar o comando 'npm run cypress' no terminal
+// Os testes foram feitos para cobrir casos de sucesso e erro no Login, tanto quanto registro, edição, busca e exclusão de modelos no banco de atividades em diferentes abordagens
+// O processo de Login está sendo executado no arquivo commands.ts
+// ***********************************************************
+
 describe('template spec', function() {
     beforeEach(function() {
       cy.visit('https://growth-station-client-git-junk-teste-qa-growthmachine.vercel.app/login')
@@ -12,15 +19,15 @@ describe('template spec', function() {
     })
     it('Error wrong login user', function() {        
         cy.fillUserLogin('testEmail@growthmachine.com.br', 'senha321')
-        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos');
+        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos')
     })
     it('Error empty password', function() {        
         cy.fillUserLogin('testEmail@growthmachine.com.br', '{backspace}')
-        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos');
+        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos')
     })
     it('Error wrong email', function() {        
         cy.fillUserLogin('testEmail.growthmachine.com.br', 'senha123')
-        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos');
+        cy.get('.text-left').should('be.visible').should('contain', '*E-mail ou senha inválidos')
     })
     it('Create a research activity template', function() {     
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
@@ -37,16 +44,16 @@ describe('template spec', function() {
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
         cy.get('main.flex').find('h1').contains('Banco de Atividades').should('be.visible')
         cy.contains('p', 'Pesquisa • Pesquisa de Empresa de Jogos').parents('[data-testid="task-list-item"]').should('exist')
-        cy.get('div.flex.flex-col.gap-4.w-full > div:nth-child(1) [data-testid="task-list-item-dropdown-trigger"]').click();
+        cy.get('div.flex.flex-col.gap-4.w-full > div:nth-child(1) [data-testid="task-list-item-dropdown-trigger"]').click()
         cy.get('[data-testid="task-list-item-dropdown-content"]').should('be.visible')
         cy.get('[data-testid="task-list-item-dropdown-duplicate"]').click()
-        cy.get('[data-testid="task-list-item"]').eq(0).should('contain', 'Pesquisa • Pesquisa de Empresa de Jogos (Cópia)');    
+        cy.get('[data-testid="task-list-item"]').eq(0).should('contain', 'Pesquisa • Pesquisa de Empresa de Jogos (Cópia)')   
     })
     it('Edit a research activity template', function() {     
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
         cy.get('main.flex').find('h1').contains('Banco de Atividades').should('be.visible')
         cy.contains('p', 'Pesquisa • Pesquisa de Empresa de Jogos (Cópia)').parents('[data-testid="task-list-item"]').should('exist')
-        cy.get('div.flex.flex-col.gap-4.w-full > div:nth-child(1) [data-testid="task-list-item-dropdown-trigger"]').click();
+        cy.get('div.flex.flex-col.gap-4.w-full > div:nth-child(1) [data-testid="task-list-item-dropdown-trigger"]').click()
         cy.get('[data-testid="task-list-item-dropdown-content"]').should('be.visible')
         cy.get('[data-testid="task-list-item-dropdown-edit"]').click()
         cy.get('input[id="title"]').clear()
@@ -54,7 +61,7 @@ describe('template spec', function() {
         cy.get('[data-testid="task-description-editor"]').clear()
         cy.get('[data-testid="task-description-editor"]').type('Procuro empresas que tenha portfólio em Redes Sociais')
         cy.get('[data-testid="task-save-button"]').click()
-        cy.get('[data-testid="task-list-item"]').eq(0).should('contain', 'Pesquisa • Pesquisa de Empresa de Marketing');  
+        cy.get('[data-testid="task-list-item"]').eq(0).should('contain', 'Pesquisa • Pesquisa de Empresa de Marketing')  
     })
     it('Research activity template: No title error', function() {  
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
@@ -63,7 +70,7 @@ describe('template spec', function() {
         cy.get('[data-testid="task-type-select"]').click()
         cy.contains('div[role="menuitem"]', 'Pesquisa').click()        
         cy.get('[data-testid="task-save-button"]').click()
-        cy.get('form.flex.flex-col.gap-6.w-full').find('div:nth-child(3) div.flex.items-center.gap-2.h-8.p-2.transition').should('exist').should('not.have.class', 'border-neutral-300').and('have.class', 'border-red-500');        
+        cy.get('form.flex.flex-col.gap-6.w-full').find('div:nth-child(3) div.flex.items-center.gap-2.h-8.p-2.transition').should('exist').should('not.have.class', 'border-neutral-300').and('have.class', 'border-red-500')        
     })  
     it('Research activity linkedin: No message error', function() {  
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
@@ -73,7 +80,7 @@ describe('template spec', function() {
         cy.contains('div[role="menuitem"]', 'LinkedIn').click()
         cy.get('input[id="title"]').type('Empresas de tecnologia avançada')        
         cy.get('button[type="submit"]').click({ force: true })
-        cy.get('[data-testid="task-social-message-error"]').should('be.visible').should('contain', '*O campo não pode ficar vazio');
+        cy.get('[data-testid="task-social-message-error"]').should('be.visible').should('contain', '*O campo não pode ficar vazio')
     })
     it('Filter Linkedin templates', function() {
         cy.fillUserLogin('alan_nichols_717@growthmachine.com.br', 'senha123')
@@ -87,7 +94,7 @@ describe('template spec', function() {
         cy.get('[data-testid="task-type-filter"]').click() 
         cy.contains('div[role="menuitem"]', 'LinkedIn').click()
         cy.get('div[data-testid="task-list-item"]').each(($item) => {
-            cy.wrap($item).find('[data-testid="task-list-item-title"]').should('contain.text', 'LinkedIn');
+            cy.wrap($item).find('[data-testid="task-list-item-title"]').should('contain.text', 'LinkedIn')
         })
     })
     it('Search "Games" templates', function() {
@@ -111,6 +118,6 @@ describe('template spec', function() {
                 .get('[data-testid="task-list-item-dropdown-delete"]').click()
             cy.wait(500);
         })
-        cy.get('[data-testid="no-filtered-tasks-message"]').should('contain.text', 'Não há registro de Atividades com os filtros selecionados.');  
+        cy.get('[data-testid="no-filtered-tasks-message"]').should('contain.text', 'Não há registro de Atividades com os filtros selecionados.') 
     })
 })
